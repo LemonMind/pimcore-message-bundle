c523369bb9bbfd03eb9d00ede0d938e18d812259
Pawel Filipkowski <p.filipkowski@lemonmind.com>
2022-08-03 10:21:03 +0200

    add package version

--- src/MessageBundle/LemonMindMessageBundle.php
+++ src/MessageBundle/LemonMindMessageBundle.php
@@ -3,9 +3,12 @@
 namespace LemonMind\MessageBundle;
 
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
 
 class LemonMindMessageBundle extends AbstractPimcoreBundle
 {
+    use PackageVersionTrait;
+
     public function getJsPaths()
     {
         return [
