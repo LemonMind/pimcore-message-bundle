75e3c98741ff0e0736b24862e2d079679b1f8531
Pawel Filipkowski <p.filipkowski@lemonmind.com>
2022-08-04 16:10:37 +0200

    fix bugs from phpstan

--- src/MessageBundle/Model/EmailMessageModel.php
+++ src/MessageBundle/Model/EmailMessageModel.php
@@ -3,25 +3,31 @@
 namespace LemonMind\MessageBundle\Model;
 
 
+use Pimcore\Model\DataObject\AbstractObject;
+
 class EmailMessageModel extends AbstractMessageModel
 {
     public function create(): string
     {
-        $html = '<table>';
-        foreach ($this->fields as $field) {
-            $data = $this->product->get($field);
-            if (null === $data) {
-                continue;
+        if ($this->product instanceof AbstractObject) {
+            $html = '<table>';
+            foreach ($this->fields as $field) {
+                $data = $this->product->get($field);
+                if (null === $data) {
+                    continue;
+                }
+                $html .= '<tr>';
+                $html .= '<td>' . $field . '</td>';
+                $html .= '<td>' . (is_scalar($data) ? $data : $data->getName()) . '</td>';
+                $html .= '</tr>';
             }
-            $html .= '<tr>';
-            $html .= '<td>' . $field . '</td>';
-            $html .= '<td>' . (is_scalar($data) ? $data : $data->getName()) . '</td>';
             $html .= '</tr>';
-        }
-        $html .= '</tr>';
-        $html .= '</table>';
-        if ($this->additionalInfo !== '') {
-            $html .= '<br>' . $this->additionalInfo;
+            $html .= '</table>';
+            if ($this->additionalInfo !== '') {
+                $html .= '<br>' . $this->additionalInfo;
+            }
+        } else {
+            $html = 'Error creating message';
         }
         return $html;
     }
--- src/MessageBundle/Model/TelegramMessageModel.php
+++ src/MessageBundle/Model/TelegramMessageModel.php
@@ -2,6 +2,7 @@
 
 namespace LemonMind\MessageBundle\Model;
 
+use Pimcore\Model\DataObject\AbstractObject;
 use Symfony\Component\Notifier\Message\ChatMessage;
 
 class TelegramMessageModel extends AbstractMessageModel
@@ -9,22 +10,25 @@ class TelegramMessageModel extends AbstractMessageModel
 
     public function create(): ChatMessage
     {
-        $subject = "Object " . $this->product->getName() . ' id ' . $this->product->getId();
-        foreach ($this->fields as $field) {
-            $data = $this->product->get($field);
-            if (null === $data) {
-                continue;
+        if ($this->product instanceof AbstractObject) {
+            $subject = "Object id " . $this->product->getId();
+            foreach ($this->fields as $field) {
+                $data = $this->product->get($field);
+                if (null === $data) {
+                    continue;
+                }
+                $subject .= "\n$field: ";
+                $subject .= is_scalar($data) ? $data : $data->getName();
             }
-            $subject .= "\n$field: ";
-            $subject .= is_scalar($data) ? $data : $data->getName();
-        }
-        if ($this->additionalInfo !== '') {
-            $subject .= "\n\nAdditional information";
-            $subject .= "\n$this->additionalInfo";
-        }
+            if ($this->additionalInfo !== '') {
+                $subject .= "\n\nAdditional information";
+                $subject .= "\n$this->additionalInfo";
+            }
 
-        $chatMessage = new ChatMessage("$subject");
-        $chatMessage->transport('telegram');
+            $chatMessage = new ChatMessage("$subject");
+            $chatMessage->transport('telegram');
+        } else
+            $chatMessage = new ChatMessage("Error creating message");
         return $chatMessage;
     }
 }
